9a32c296217f381b46aadac046986946285416ec
RAFI-2019 <kmrafiqulalam2008@gmail.com>
2023-12-04 00:49:28 +0600

    ./src/data/CandidatesData.js

---
+++ src/data/CandidatesData.js
@@ -0,0 +1,110 @@
+const candidatesData = [
+    {
+      name: "Jamil",
+      generalInformation: {
+        candidateName: "Jamil",
+        address: "123 Main St, Cityville",
+        education: "PhD",
+        presentLegalAllegations: "None",
+        previousLegalAllegations: "Traffic Violation",
+        profession: "Software Engineer",
+        sourceOfIncome: "Employment",
+      },
+      assetsAndLiabilities: {
+        tangibleAssets: "Car, Laptop",
+        intangibleAssets: "Stocks, Patents",
+        liabilitiesDebts: "Student Loan",
+      },
+      criminalCases: {
+        historicalMPStatus: "Not applicable",
+        loanInfo: "No outstanding loans",
+      },
+    },
+    {
+      name: "Reza",
+      generalInformation: {
+        candidateName: "Reza",
+        address: "456 Oak St, Townsville",
+        education: "H.S.C",
+        presentLegalAllegations: "Shoplifting",
+        previousLegalAllegations: "None",
+        profession: "Business Analyst",
+        sourceOfIncome: "Salary, Investments",
+      },
+      assetsAndLiabilities: {
+        tangibleAssets: "House, Car",
+        intangibleAssets: "Savings, Cryptocurrency",
+        liabilitiesDebts: "Mortgage, Credit Card Debt",
+      },
+      criminalCases: {
+        historicalMPStatus: "No history",
+        loanInfo: "Personal Loan",
+      },
+    },
+    {
+      name: "Rezwaan",
+      generalInformation: {
+        candidateName: "Rezwaan",
+        address: "789 Pine St, Villagetown",
+        education: "Bachelor's",
+        presentLegalAllegations: "None",
+        previousLegalAllegations: "None",
+        profession: "Financial Analyst",
+        sourceOfIncome: "Salary, Investments",
+      },
+      assetsAndLiabilities: {
+        tangibleAssets: "Apartment, Car",
+        intangibleAssets: "Stocks, Retirement Account",
+        liabilitiesDebts: "None",
+      },
+      criminalCases: {
+        historicalMPStatus: "Not applicable",
+        loanInfo: "Auto Loan",
+      },
+    },
+    {
+      name: "Richa",
+      generalInformation: {
+        candidateName: "Richa",
+        address: "101 Maple St, Grovetown",
+        education: "Master's",
+        presentLegalAllegations: "None",
+        previousLegalAllegations: "None",
+        profession: "Marketing Manager",
+        sourceOfIncome: "Salary, Consulting",
+      },
+      assetsAndLiabilities: {
+        tangibleAssets: "House, Jewelry",
+        intangibleAssets: "Savings, Investments",
+        liabilitiesDebts: "Mortgage",
+      },
+      criminalCases: {
+        historicalMPStatus: "Not applicable",
+        loanInfo: "Credit Card Debt",
+      },
+    },
+    {
+      name: "Roma",
+      generalInformation: {
+        candidateName: "Roma",
+        address: "567 Elm St, Parkville",
+        education: "Bachelor's",
+        presentLegalAllegations: "None",
+        previousLegalAllegations: "None",
+        profession: "Counselor",
+        sourceOfIncome: "Salary",
+      },
+      assetsAndLiabilities: {
+        tangibleAssets: "Car, Electronics",
+        intangibleAssets: "Savings",
+        liabilitiesDebts: "Student Loan",
+      },
+      criminalCases: {
+        historicalMPStatus: "Not applicable",
+        loanInfo: "Personal Loan",
+      },
+    },
+  ];
+  
+  export default candidatesData;
+  
